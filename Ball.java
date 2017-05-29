78692d984c3c84e07778c9b2d042b1c7c07f921a
William010x <william010x@gmail.com>
2017-05-29 09:12:49 -0400

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -1,22 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+
+/** Ball Class
+  * Creates component that looks like a ball
+  * @since 5/29/17
+  * @author William San
+  */
 public class Ball extends JComponent
 {
+  /**
+    * Default Constructor
+    */
   public Ball ()
   {
     super ();
     this.setPreferredSize(new Dimension(300,300));
   }
   
+  /** 
+    * Draws a ball component
+    * @overrides paintComponent
+    * @param g The graphics context where the component should be drawn
+    */
   public void paintComponent(Graphics g)
   {
     super.paintComponent(g);
-  
-    //head
+    
     g.setColor(Color.BLACK);
     g.fillOval(19,19,93,93);
     g.setColor(Color.YELLOW);
     g.fillOval(20,20,90,90); 
   }
-}      
-
+}
