cd04a5957b408247ff2500926defb3ce1b1c9c90
William010x <william010x@gmail.com>
2017-05-29 09:02:01 -0400

    Update StartController.java

--- StartController.java
+++ StartController.java
@@ -3,23 +3,32 @@ import java.awt.event.*;
 
 /** StartController class
   * Operates the operation buttons
-  * @since 4/24/2017
+  * @since 5/29/2017
   * @author Nicolas Chatziargilliou
   */
 public class StartController implements ActionListener
 {
+  private JTextField angle;          //Initial velocity's angle above or below the horizontal (above +, below -) (degrees)
+  private JTextField displacementY;  //Vertical displacement of projectile (m)
   
-  private Calculation model;
-  
-  
-  
-  public StartController(Calculation aModel)
+   /** 
+   * Default constructor
+   * Links the component to the Model
+   * @param theta1 Iniital velocity's angle (above horizontal +, below horizontal -) (degrees)
+   * @param dY Vertical displacement of the projectile (m)
+   */ 
+  public StartController(JTextField theta, JTextField dY)
   {
-    this.model = aModel;
+    this.angle = theta;
+    this.displacementY = dY;
   }
   
+  /** 
+   * Calculates all values (initial and final situation)
+   * @param e The event sent from the button
+   */
   public void actionPerformed(ActionEvent e)
   {
-    
+    Animation.start(angle.getText(), displacementY.getText());
   }
 }
