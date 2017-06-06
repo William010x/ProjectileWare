d21e67eaa9ac0182e743baf384763209a40e3bc7
William010x <william010x@gmail.com>
2017-06-05 21:56:42 -0400

    Update ResetController.java

--- Complete/ResetController.java
+++ Complete/ResetController.java
@@ -1,30 +1,24 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-/** 
- * ResetController Class
+/** ResetController Class
  * The controller for the reset button (to clear all values)
  * @author William San
- * @since 5/31/17
- */ 
+ * @since 5/31/17 */ 
 public class ResetController implements ActionListener
 {
   private Calculation model;         //The Model storing all numbers
   
-  /** 
-   * Default constructor
+  /** Default constructor
    * Links model to Reset button
-   * @param aModel The Model storing all the numbers
-   */ 
+   * @param aModel The Model storing all the numbers */ 
   public ResetController(Calculation model)
   {
     this.model = aModel;
   }
   
-  /** 
-   * Resets all input boxes
-   * @param e The event sent from the button
-   */
+  /** Resets all input boxes
+   * @param e The event sent from the button */
   public void actionPerformed(ActionEvent e)
   {
     this.model.reset();
