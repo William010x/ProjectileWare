912edc3e88320646c870fa36d900a0dcb987ae99
therealafinc <therealafinc@gmail.com>
2017-06-15 09:23:55 -0400

    Add files via upload

--- Animation.java
+++ Animation.java
@@ -36,43 +36,28 @@ private Calculation model;
     
     g.setColor(Color.BLACK);
     g.fillOval(0 + positionX, 125 + positionY, 20, 20);
+    System.out.println("Test");
    }
-  
-  /** Calls the moveVertical and moveHorizontal methods */
-  public void start()
-  {
-    //Moves object in 0.1 sec increments until the maximum time value is reached
-    for (double t = 0; t < this.model.getTime(); t += 0.01)
-    {
-      moveHorizontal(t);
-      moveVertical(t);
-      repaint();
-      
-      try
-      {
-        Thread.currentThread( ).sleep(2);
-      } catch (InterruptedException ie) {
-      }
-    }
-  }
   
   /** Calculates the object's new horizontal position
     * @param       the current time at object's position */
-  private void moveHorizontal(double time)
+  public void moveHorizontal(double time)
   {
     this.positionY = (int)((this.model.getVelocity1() * Math.sin(Math.toRadians(this.model.getAngle1())) * time) + (0.5 * -9.81 * Math.pow(time, 2)));
+    this.paintImmediately(0, 0, 500, 500);
   }
   
   /** Calculates the object's new vertical position
     * @param       the current time at object's position */
-  private void moveVertical(double time)
+  public void moveVertical(double time)
   {
     this.positionX = (int)(this.model.getVelocity1() * Math.cos(Math.toRadians(this.model.getAngle1())) * time);
+    this.paintImmediately(0, 0, 500, 500);
   }
   
   /** Clears the animation screen */
   public void reset()
   {
-    repaint();
+    this.repaint();
   }
 }
