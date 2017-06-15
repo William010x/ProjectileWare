4620d7a1cf7ac30da6168d7753f586cd5bf1223d
izanagiNoOkami <fortressmaximuss@gmail.com>
2017-06-15 09:40:28 -0400

    Add files via upload

--- Animation.java
+++ Animation.java
@@ -41,15 +41,15 @@ private Calculation model;
   
   /** Calculates the object's new horizontal position
     * @param       the current time at object's position */
-  public void moveHorizontal(double time)
+  public void moveVertical(double time)
   {
-    this.positionY = (int)Math.round(((this.model.getVelocity1() * Math.sin(Math.toRadians(this.model.getAngle1())) * time) + (0.5 * -9.81 * Math.pow(time, 2))));
+    this.positionY = (int)Math.round(-((this.model.getVelocity1() * Math.sin(Math.toRadians(this.model.getAngle1())) * time) + (0.5 * -9.81 * Math.pow(time, 2))));
     this.paintImmediately(0, 0, 500, 500);
   }
   
   /** Calculates the object's new vertical position
     * @param       the current time at object's position */
-  public void moveVertical(double time)
+  public void moveHorizontal(double time)
   {
     this.positionX = (int)Math.round((this.model.getVelocity1() * Math.cos(Math.toRadians(this.model.getAngle1())) * time));
     this.paintImmediately(0, 0, 500, 500);
